cf2ac17caf7a43ee5aab7f57dd9a7ddb6d248246
alexeygridnev <alexeygridnev93@gmail.com>
2016-10-25 16:24:51 +0200

    Create textdata.py

---
+++ textdata.py
@@ -0,0 +1,137 @@
+
+#dictionary with region names and website URLs:
+
+listdict={'''Республика Адыгея''': '''http://www.adygei.vybory.izbirkom.ru/region/adygei?action=show&global=1&vrn=100100067795849&region=1&prver=0&pronetvd=0''',
+'''Республика Алтай''':	'''http://www.altai_rep.vybory.izbirkom.ru/region/altai_rep?action=show&global=1&vrn=100100067795849&region=2&prver=0&pronetvd=0''',
+'''Республика Башкортостан''':	'''http://www.bashkortostan.vybory.izbirkom.ru/region/bashkortostan?action=show&global=1&vrn=100100067795849&region=3&prver=0&pronetvd=0''',
+'''Республика Бурятия''': '''http://www.buriat.vybory.izbirkom.ru/region/buriat?action=show&global=1&vrn=100100067795849&region=4&prver=0&pronetvd=0''',
+'''Республика Дагестан''':  '''http://www.dagestan.vybory.izbirkom.ru/region/dagestan?action=show&global=1&vrn=100100067795849&region=5&prver=0&pronetvd=0''',
+'''Республика Ингушетия''': '''http://www.ingush.vybory.izbirkom.ru/region/ingush?action=show&global=1&vrn=100100067795849&region=6&prver=0&pronetvd=0''',
+'''Кабардино-Балкарская республика''':	'''http://www.kabardin-balkar.vybory.izbirkom.ru/region/kabardin-balkar?action=show&global=1&vrn=100100067795849&region=7&prver=0&pronetvd=0''',
+'''Республика Калмыкия''':  '''http://www.kalmyk.vybory.izbirkom.ru/region/kalmyk?action=show&global=1&vrn=100100067795849&region=8&prver=0&pronetvd=0''',
+'''Карачаево-Черкесская республика''':	'''http://www.karachaev-cherkess.vybory.izbirkom.ru/region/karachaev-cherkess?action=show&global=1&vrn=100100067795849&region=9&prver=0&pronetvd=0''',
+'''Республика Карелия''':   '''http://www.karel.vybory.izbirkom.ru/region/karel?action=show&global=1&vrn=100100067795849&region=10&prver=0&pronetvd=0''',
+'''Республика Коми''':	'''http://www.komi.vybory.izbirkom.ru/region/komi?action=show&global=1&vrn=100100067795849&region=11&prver=0&pronetvd=0''',
+'''Республика Крым''':	'''http://www.crimea.vybory.izbirkom.ru/region/crimea?action=show&global=1&vrn=100100067795849&region=93&prver=0&pronetvd=0''',
+'''Республика Мордовия''':  '''http://www.mordov.vybory.izbirkom.ru/region/mordov?action=show&root_a=132000001&vrn=100100067795849&region=13&global=true&type=0&sub_region=13&root=1000032&prver=0&pronetvd=0&tvd=100100067795886''',
+'''Республика Саха(Якутия)''':	'''http://www.yakut.vybory.izbirkom.ru/region/yakut?action=show&global=1&vrn=100100067795849&region=14&prver=0&pronetvd=0''',
+'''Республика Северная Осетия-Алания''': '''http://www.n_osset-alania.vybory.izbirkom.ru/region_osset-alania?action=show&global=1&vrn=100100067795849&region=15&prver=0&pronetvd=0''',
+'''Республика Татарстан''': '''http://www.tatarstan.vybory.izbirkom.ru/region/tatarstan?action=show&root_a=162000009&vrn=100100067795849&region=16&global=true&type=0&sub_region=16&root=1000038&prver=0&pronetvd=0&tvd=100100067795892''',
+'''Республика Тыва''':	'''http://www.tyva.vybory.izbirkom.ru/region/tyva?action=show&global=1&vrn=100100067795849&region=17&prver=0&pronetvd=0''',
+'''Удмуртская республика''':'''http://www.udmurt.vybory.izbirkom.ru/region/udmurt?action=show&global=1&vrn=100100067795849&region=18&prver=0&pronetvd=0''',
+'''Республика Хакасия''':   '''http://www.khakas.vybory.izbirkom.ru/region/khakas?action=show&global=1&vrn=100100067795849&region=19&prver=0&pronetvd=0''',
+'''Чеченская республика''': '''http://www.chechen.vybory.izbirkom.ru/region/chechen?action=show&root_a=202000021&vrn=100100067795849&region=20&global=true&type=0&sub_region=20&root=1000052&prver=0&pronetvd=0&tvd=100100067795906''',
+'''Чувашская республика''': '''http://www.chuvash.vybory.izbirkom.ru/region/chuvash?action=show&global=1&vrn=100100067795849&region=21&prver=0&pronetvd=0''',
+'''Алтайский край''':	'''http://www.altai_terr.vybory.izbirkom.ru/region/altai_terr?action=show&global=1&vrn=100100067795849&region=22&prver=0&pronetvd=0''',
+'''Забайкальский край''':   '''http://www.zabkray.vybory.izbirkom.ru/region/zabkray?action=show&global=1&vrn=100100067795849&region=92&prver=0&pronetvd=0''',
+'''Камчатский край''':	'''http://www.kamchatka_krai.vybory.izbirkom.ru/region/kamchatka_krai?action=show&global=1&vrn=100100067795849&region=91&prver=0&pronetvd=0''',
+'''Краснодарский край''':   '''http://www.krasnodar.vybory.izbirkom.ru/region/krasnodar?action=show&global=1&vrn=100100067795849&region=23&prver=0&pronetvd=0''',
+'''Красноярский край''':    '''http://www.krasnoyarsk.vybory.izbirkom.ru/region/krasnoyarsk?action=show&global=1&vrn=100100067795849&region=24&prver=0&pronetvd=0''',
+'''Пермский край''':	'''http://www.permkrai.vybory.izbirkom.ru/region/permkrai?action=show&global=1&vrn=100100067795849&region=90&prver=0&pronetvd=0''',
+'''Приморский край''':	'''http://www.primorsk.vybory.izbirkom.ru/region/primorsk?action=show&global=1&vrn=100100067795849&region=25&prver=0&pronetvd=0''',
+'''Ставропольский край''':  '''http://www.stavropol.vybory.izbirkom.ru/region/stavropol?action=show&global=1&vrn=100100067795849&region=26&prver=0&pronetvd=0''',
+'''Хабаровский край''':	'''http://www.khabarovsk.vybory.izbirkom.ru/region/khabarovsk?action=show&global=1&vrn=100100067795849&region=27&prver=0&pronetvd=0''',
+'''Амурская область''':	'''http://www.amur.vybory.izbirkom.ru/region/amur?action=show&global=1&vrn=100100067795849&region=28&prver=0&pronetvd=0''',
+'''Архангельская область''': '''http://www.arkhangelsk.vybory.izbirkom.ru/region/arkhangelsk?action=show&global=1&vrn=100100067795849&region=29&prver=0&pronetvd=0''',
+'''Астраханская область''':  '''http://www.astrakhan.vybory.izbirkom.ru/region/astrakhan?action=show&global=1&vrn=100100067795849&region=30&prver=0&pronetvd=0''',
+'''Белгородская область''':  '''http://www.belgorod.vybory.izbirkom.ru/region/belgorod?action=show&global=1&vrn=100100067795849&region=31&prver=0&pronetvd=0''',
+'''Брянская область''':	'''http://www.bryansk.vybory.izbirkom.ru/region/bryansk?action=show&global=1&vrn=100100067795849&region=32&prver=0&pronetvd=0''',
+'''Владимирская область''':  '''http://www.vladimir.vybory.izbirkom.ru/region/vladimir?action=show&global=1&vrn=100100067795849&region=33&prver=0&pronetvd=0''',
+'''Волгоградская область''': '''http://www.volgograd.vybory.izbirkom.ru/region/volgograd?action=show&global=1&vrn=100100067795849&region=34&prver=0&pronetvd=0''',
+'''Вологодская область''':   '''http://www.vologod.vybory.izbirkom.ru/region/vologod?action=show&global=1&vrn=100100067795849&region=35&prver=0&pronetvd=0''',
+'''Воронежская область''':  '''http://www.voronezh.vybory.izbirkom.ru/region/voronezh?action=show&global=1&vrn=100100067795849&region=36&prver=0&pronetvd=0''',
+'''Ивановская область''':   '''http://www.ivanovo.vybory.izbirkom.ru/region/ivanovo?action=show&global=1&vrn=100100067795849&region=37&prver=0&pronetvd=0''',
+'''Иркутская область''':    '''http://www.irkutsk.vybory.izbirkom.ru/region/irkutsk?action=show&global=1&vrn=100100067795849&region=38&prver=0&pronetvd=0''',
+'''Калининградская область''':	'''http://www.kaliningrad.vybory.izbirkom.ru/region/kaliningrad?action=show&global=1&vrn=100100067795849&region=39&prver=0&pronetvd=0''',
+'''Калужская область''':    '''http://www.kaluga.vybory.izbirkom.ru/region/kaluga?action=show&global=1&vrn=100100067795849&region=40&prver=0&pronetvd=0''',
+'''Кемеровская область''':  '''http://www.kemerovo.vybory.izbirkom.ru/region/kemerovo?action=show&global=1&vrn=100100067795849&region=42&prver=0&pronetvd=0''',
+'''Кировская область''':    '''http://www.kirov.vybory.izbirkom.ru/region/kirov?action=show&global=1&vrn=100100067795849&region=43&prver=0&pronetvd=0''',
+'''Костромская область''':  '''http://www.kostroma.vybory.izbirkom.ru/region/kostroma?action=show&global=1&vrn=100100067795849&region=44&prver=0&pronetvd=0''',
+'''Курганская область''':   '''http://www.kurgan.vybory.izbirkom.ru/region/kurgan?action=show&root_a=1000141&vrn=100100067795849&region=45&global=true&type=0&sub_region=45&root=1000141&prver=0&pronetvd=0&tvd=100100067795995''',
+'''Курская область''':	'''http://www.kursk.vybory.izbirkom.ru/region/kursk?action=show&global=1&vrn=100100067795849&region=46&prver=0&pronetvd=0''',
+'''Ленинградская область''': '''http://www.leningrad-reg.vybory.izbirkom.ru/region/leningrad-reg?action=show&global=1&vrn=100100067795849&region=47&prver=0&pronetvd=0''',
+'''Липецкая область''':	'''http://www.lipetsk.vybory.izbirkom.ru/region/lipetsk?action=show&global=1&vrn=100100067795849&region=48&prver=0&pronetvd=0''',
+'''Магаданская область''':  '''http://www.magadan.vybory.izbirkom.ru/region/magadan?action=show&global=1&vrn=100100067795849&region=49&prver=0&pronetvd=0''',
+'''Московская область''':   '''http://www.moscow_reg.vybory.izbirkom.ru/region/moscow_reg?action=show&global=1&vrn=100100067795849&region=50&prver=0&pronetvd=0''',
+'''Мурманская область''':   '''http://www.murmansk.vybory.izbirkom.ru/region/murmansk?action=show&global=1&vrn=100100067795849&region=51&prver=0&pronetvd=0''',
+'''Нижегородская область''': '''http://www.nnov.vybory.izbirkom.ru/regionnov?action=show&global=1&vrn=100100067795849&region=52&prver=0&pronetvd=0''',
+'''Новгородская область''':  '''http://www.novgorod.vybory.izbirkom.ru/regionovgorod?action=show&global=1&vrn=100100067795849&region=53&prver=0&pronetvd=0''',
+'''Новосибирская область''':	'''http://www.novosibirsk.vybory.izbirkom.ru/regionovosibirsk?action=show&global=1&vrn=100100067795849&region=54&prver=0&pronetvd=0''',
+'''Омская область''':	'''http://www.omsk.vybory.izbirkom.ru/region/omsk?action=show&global=1&vrn=100100067795849&region=55&prver=0&pronetvd=0''',
+'''Оренбургская область''':	'''http://www.orenburg.vybory.izbirkom.ru/region/orenburg?action=show&global=1&vrn=100100067795849&region=56&prver=0&pronetvd=0''',
+'''Орловская область''':	'''http://www.orel.vybory.izbirkom.ru/region/orel?action=show&global=1&vrn=100100067795849&region=57&prver=0&pronetvd=0''',
+'''Пензенская область''':	'''http://www.penza.vybory.izbirkom.ru/region/penza?action=show&global=1&vrn=100100067795849&region=58&prver=0&pronetvd=0''',
+'''Псковская область''':	'''http://www.pskov.vybory.izbirkom.ru/region/pskov?action=show&root_a=1000195&vrn=100100067795849&region=60&global=true&type=0&sub_region=60&root=1000195&prver=0&pronetvd=0&tvd=100100067796049''',
+'''Ростовская область''':	'''http://www.rostov.vybory.izbirkom.ru/region/rostov?action=show&global=1&vrn=100100067795849&region=61&prver=0&pronetvd=0''',
+'''Рязанская область''':	'''http://www.ryazan.vybory.izbirkom.ru/region/ryazan?action=show&global=1&vrn=100100067795849&region=62&prver=0&pronetvd=0''',
+'''Самарская область''':	'''http://www.samara.vybory.izbirkom.ru/region/samara?action=show&global=1&vrn=100100067795849&region=63&prver=0&pronetvd=0''',
+'''Саратовская область''':	'''http://www.saratov.vybory.izbirkom.ru/region/saratov?action=show&global=1&vrn=100100067795849&region=64&prver=0&pronetvd=0''',
+'''Сахалинская область''':	'''http://www.sakhalin.vybory.izbirkom.ru/region/sakhalin?action=show&global=1&vrn=100100067795849&region=65&prver=0&pronetvd=0''',
+'''Свердловская область''':	'''http://www.sverdlovsk.vybory.izbirkom.ru/region/sverdlovsk?action=show&global=1&vrn=100100067795849&region=66&prver=0&pronetvd=0''',
+'''Смоленская область''':	'''http://www.smolensk.vybory.izbirkom.ru/region/smolensk?action=show&global=1&vrn=100100067795849&region=67&prver=0&pronetvd=0''',
+'''Тамбоская область''':	'''http://www.tambov.vybory.izbirkom.ru/region/tambov?action=show&global=1&vrn=100100067795849&region=68&prver=0&pronetvd=0''',
+'''Тверская область''':	'''http://www.tver.vybory.izbirkom.ru/region/tver?action=show&root_a=1000235&vrn=100100067795849&region=69&global=true&type=0&sub_region=69&root=1000235&prver=0&pronetvd=0&tvd=100100067796089''',
+'''Томская область''':	'''http://www.tomsk.vybory.izbirkom.ru/region/tomsk?action=show&global=1&vrn=100100067795849&region=70&prver=0&pronetvd=0''',
+'''Тульская область''':	'''http://www.tula.vybory.izbirkom.ru/region/tula?action=show&global=1&vrn=100100067795849&region=71&prver=0&pronetvd=0''',
+'''Тюменская область''':	'''http://www.tyumen.vybory.izbirkom.ru/region/tyumen?action=show&global=1&vrn=100100067795849&region=72&prver=0&pronetvd=0''',
+'''Ульяновская область''':	'''http://www.ulyanovsk.vybory.izbirkom.ru/region/ulyanovsk?action=show&global=1&vrn=100100067795849&region=73&prver=0&pronetvd=0''',
+'''Челябинская область''':	'''http://www.chelyabinsk.vybory.izbirkom.ru/region/chelyabinsk?action=show&global=1&vrn=100100067795849&region=74&prver=0&pronetvd=0''',
+'''Ярославская область''':	'''http://www.yaroslavl.vybory.izbirkom.ru/region/yaroslavl?action=show&global=1&vrn=100100067795849&region=76&prver=0&pronetvd=0''',
+'''Москва''':	'''http://www.moscow_city.vybory.izbirkom.ru/region/moscow_city?action=show&global=1&vrn=100100067795849&region=77&prver=0&pronetvd=0''',
+'''Санкт-Петербург''':	'''http://www.st-petersburg.vybory.izbirkom.ru/region/st-petersburg?action=show&global=1&vrn=100100067795849&region=78&prver=0&pronetvd=0''',
+'''Севастополь''':	'''http://www.sevastopol.vybory.izbirkom.ru/region/sevastopol?action=show&global=1&vrn=100100067795849&region=94&prver=0&pronetvd=0''',
+'''Еврейская Авт. Обл.''':	'''http://www.jewish_aut.vybory.izbirkom.ru/region/jewish_aut?action=show&global=1&vrn=100100067795849&region=79&prver=0&pronetvd=0''',
+'''Ненецкий АО''':	'''http://www.nenetsk.vybory.izbirkom.ru/regionenetsk?action=show&root_a=1000287&vrn=100100067795849&region=83&global=true&type=0&sub_region=83&root=1000286&prver=0&pronetvd=0&tvd=100100067796140''',
+'''Ханты-Манскийский АО''':	'''http://www.khantu-mansy.vybory.izbirkom.ru/region/khantu-mansy?action=show&global=1&vrn=100100067795849&region=86&prver=0&pronetvd=0''',
+'''Чукотский АО''':	'''http://www.chukot.vybory.izbirkom.ru/region/chukot?action=show&global=1&vrn=100100067795849&region=87&prver=0&pronetvd=0''',
+'''Ямало-Ненецкий АО''':	'''http://www.yamal-nenetsk.vybory.izbirkom.ru/region/yamal-nenetsk?action=show&global=1&vrn=100100067795849&region=89&prver=0&pronetvd=0''',
+}
+
+textpr = '''v1      Номер УИК 
+v2 	Число избирателей, внесенных в список избирателей на момент окончания голосования 
+v3 	Число избирательных бюллетеней, полученных участковой избирательной комиссией
+v4 	Число избирательных бюллетеней, выданных избирателям, проголосовавшим досрочно 
+v5 	Число избирательных бюллетеней, выданных в помещении для голосования в день голосования 	
+v6 	Число избирательных бюллетеней, выданных вне помещения для голосования в день голосования 
+v7 	Число погашенных избирательных бюллетеней 
+v8 	Число избирательных бюллетеней, содержащихся в переносных ящиках для голосования  
+v9 	Число избирательных бюллетеней, содержащихся в стационарных ящиках для голосования 
+v10 	Число недействительных избирательных бюллетеней 
+v11 	Число действительных избирательных бюллетеней 
+v12 	Число открепительных удостоверений, полученных участковой избирательной комиссией 
+v13 	Число открепительных удостоверений, выданных на избирательном участке до дня голосования 
+v14 	Число избирателей, проголосовавших по открепительным удостоверениям на избирательном участке 
+v15 	Число погашенных неиспользованных открепительных удостоверений
+v16 	Число открепительных удостоверений, выданных избирателям территориальной избирательной комиссией
+v17 	Число утраченных открепительных удостоверений 
+v18 	Число утраченных избирательных бюллетеней 
+v19     Число избирательных бюллетеней, не учтённых при получении 
+	 
+v20 "РОДИНА"(abs) 
+v20.1 "РОДИНА"(%)
+v21 КОММУНИСТЫ РОССИИ (abs)
+v21.1 КОММУНИСТЫ РОССИИ(%)
+v22 "Российская партия пенсионеров за справедливость"(abs)
+v22.1 "Российская партия пенсионеров за справедливость"(%)
+v23 ЕДИНАЯ РОССИЯ(abs)
+v23.1 ЕДИНАЯ РОССИЯ(%)
+v24 "Зеленые"(abs)
+v24.1 "Зеленые"(%)
+v25 "Гражданская Платформа"(abs)
+v25.1 "Гражданская Платформа"(%)
+v26 ЛДПР(abs)
+v26.1 ЛДПР(%)
+v27 ПАРНАС(abs)
+v27.1 ПАРНАС(%)
+v28 ПАРТИЯ РОСТА(abs)
+v28.1 ПАРТИЯ РОСТА(%)
+v29 Гражданская Сила(abs)
+v29.1 Гражданская Сила(%)
+v30 ЯБЛОКО(abs)
+v30.1 ЯБЛОКО(%)
+v31 КПРФ(abs)
+v31.1 КПРФ(%)
+v32 ПАТРИОТЫ РОССИИ(abs)
+v32.1 ПАТРИОТЫ РОССИИ(%)
+v33 CP(abs)
+v33.1 CP(%)'''
